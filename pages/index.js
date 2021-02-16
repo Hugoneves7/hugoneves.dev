1db8b965c0ea004ceaa431ec4f8b05451064e746
Hugo Neves <hugo.neves@outlook.com>
2021-02-16 18:00:08 -0300

    initial version

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import getUser from '../utils/getUser'
+
+const Index = ({ repos, user }) => {
+  //JSX
+  return (
+    <div className='container mx-auto'>
+      <h1 className="text-5xl">Olá, eu sou o Hugo Neves!</h1>
+      <h2 className='font-bold text-3xl'>Meus repositórios no Github</h2>
+      <p>Github status: public repos: {user.public_repos} / public_gists: {user.public_gists} / followers: {user.followers}</p>
+      {repos.map(repo => {
+        return (
+          <div key={repo.id} className='rounded bg-gray-200 mx-8 my-4 p-4 hover:shadow-md'>
+            <h3 className='font-bold'>{repo.full_name}</h3>
+            <p>Language: {repo.language} / Stars: {repo.stargazers_count}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+export async function getServerSideProps(context) {
+  const { repos, user } = await getUser('hugoneves7')
+  return {
+    props: {
+      currentDate: new Date().toString(),
+      repos,
+      user
+    }
+  }
+}
+export default Index
